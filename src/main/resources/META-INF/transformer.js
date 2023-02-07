66123ef8f145d3a53c0b13ea87fc95fa388fb9b1
ZekerZhayard <trees1000@qq.com>
2023-02-07 20:33:08 +0800

    1.1

--- src/main/resources/META-INF/transformer.js
+++ src/main/resources/META-INF/transformer.js
@@ -1,9 +1,13 @@
 
 var ASMAPI = Java.type("net.minecraftforge.coremod.api.ASMAPI");
 var Opcodes = Java.type("org.objectweb.asm.Opcodes");
+var InsnList = Java.type("org.objectweb.asm.tree.InsnList");
 var InsnNode = Java.type("org.objectweb.asm.tree.InsnNode");
+var JumpInsnNode = Java.type("org.objectweb.asm.tree.JumpInsnNode");
+var LabelNode = Java.type("org.objectweb.asm.tree.LabelNode");
 var MethodInsnNode = Java.type("org.objectweb.asm.tree.MethodInsnNode");
 var TypeInsnNode = Java.type("org.objectweb.asm.tree.TypeInsnNode");
+var VarInsnNode = Java.type("org.objectweb.asm.tree.VarInsnNode");
 
 function initializeCoreMod() {
     return {
@@ -25,6 +29,25 @@ function initializeCoreMod() {
                         mn.instructions.insertBefore(node, new MethodInsnNode(Opcodes.INVOKESPECIAL, "java/util/concurrent/ConcurrentHashMap", "<init>", "()V", false));
                     }
                 }
+                return mn;
+            }
+        },
+        "SoundEngine_func_217933_b": {
+            "target": {
+                "type": "METHOD",
+                "class": "net/minecraft/client/audio/SoundEngine",
+                "methodName": "func_217933_b",
+                "methodDesc": "(Lnet/minecraft/client/audio/ISound;)Z"
+            },
+            "transformer": function (mn) {
+                var il = new InsnList();
+                var ln = new LabelNode();
+                il.add(new VarInsnNode(Opcodes.ALOAD, 1));
+                il.add(new JumpInsnNode(Opcodes.IFNONNULL, ln));
+                il.add(new InsnNode(Opcodes.ICONST_0));
+                il.add(new InsnNode(Opcodes.IRETURN));
+                il.add(ln);
+                mn.instructions.insert(il);
                 return mn;
             }
         }
